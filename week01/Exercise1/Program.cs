d83933288dba87e6ef1da13fad28615436145775
mememry <memry@byupathway.edu>
2025-07-05 20:59:07 -0700

    Finished Assignment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
+
+
     }
 }
